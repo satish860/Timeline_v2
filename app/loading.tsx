7ea749154c35edc65b10042c9f8d8f0e3095b788
Nandhni G A <nandhni.25@gmail.com>
2024-11-04 11:45:39 +0700

    removed loading page code and fixed eslint type error

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,19 +2,5 @@ export default function Loading() {
   // Stack uses React Suspense, which will render this page while user data is being fetched.
   // See: https://nextjs.org/docs/app/api-reference/file-conventions/loading
   return <>
-    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-      <div className="flex space-x-2">
-        {[0, 1, 2].map((index) => (
-          <div
-            key={index}
-            className="w-4 h-4 bg-green-400 rounded-full animate-grow"
-            style={{ animationDelay: `${index * 0.15}s` }}
-          ></div>
-        ))}
-      </div>
-      <p className="mt-4 text-lg font-semibold text-gray-700">
-        Loading Page...
-      </p>
-    </div>
   </>;
 }
