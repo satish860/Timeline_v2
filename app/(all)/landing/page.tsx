8b80d9b3c724176fa542e3184340e4b0dd7e3816
Poorna Prakash <jimpoorna@gmail.com>
2024-11-05 19:05:48 +0530

    Created case timeline page and removed timeline progress page.

--- app/(all)/landing/page.tsx
+++ app/(all)/landing/page.tsx
@@ -1,45 +1,24 @@
-// import TimelineTable from "@/components/TableComponent";
-// import { FileUrls } from "@/src/types";
+// import ProgressComponent from "@/components/ProgressComponent";
 // import { getXataClient } from "@/src/xata";
-// import axios from "axios";
 
 // const xata = getXataClient();
 
-// async function getCaseTimeline(id: string) {
-//   try {
-//     const record = await xata.db.timeline_Job_Queue.read(id);
-
-//     if (!record || typeof record.Timeline_url !== "string") {
-//       throw new Error("Invalid record, Timeline URL, or workspace");
-//     }
-//     const response = await axios.get(record.Timeline_url);
-
-//     const fileRecords = await xata.db.timeline_files_data
-//       .filter({
-//         "workspace.id": id,
-//       })
-//       .getAll();
-
-//     const fileUrls: FileUrls = {};
-
-//     fileRecords.forEach((file) => {
-//       if (file.file_name && file.file_url) {
-//         fileUrls[file.file_name] = file.file_url;
-//       }
-//     });
-
-//     return {
-//       pdfUrls: fileUrls,
-//       events: response.data.events,
-//     };
-//   } catch (error) {
-//     console.error("Error fetching timeline:", error);
-//     throw error;
+// async function getCaseData(id: string) {
+//   const record = await xata.db.timeline_Job_Queue.read(id);
+//   if (!record || typeof record.CaseName !== "string") {
+//     throw new Error("Case not found");
 //   }
+//   return {
+//     name: record.CaseName,
+//   };
 // }
 
-// export default async function Timeline({ params }: { params: { id: string } }) {
-//   const { events, pdfUrls } = await getCaseTimeline(params.id);
+// export default async function TimelineProgress({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   const { name } = await getCaseData(params.id);
 
-//   return <TimelineTable data={events} pdfUrls={pdfUrls} />;
+//   return <ProgressComponent caseid={params.id} name={name} />;
 // }
