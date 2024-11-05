1b62dc14c63b0f834cff8b87a8946482b9aca40d
Poorna Prakash <jimpoorna@gmail.com>
2024-11-05 17:10:00 +0530

    Added features and footer section.

--- app/(all)/landing/page.tsx
+++ app/(all)/landing/page.tsx
@@ -1,5 +1,15 @@
+import { FeaturesSection } from "@/components/layout/features";
+import { FooterSection } from "@/components/layout/footer";
 import { HeroSection } from "@/components/layout/hero";
+import { TestimonialSection } from "@/components/layout/testimonials";
 
 export default function LandingPage() {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <TestimonialSection />
+      <FooterSection />
+    </>
+  );
 }
