ab788a1ca5ef07a5313b04e6f33559264eb02524
Poorna Prakash <jimpoorna@gmail.com>
2024-11-05 10:21:12 +0530

    Updated the progress page route.

---
+++ app/timelineprogress/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default function TimelineProgress({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div>
+      <h1>Hello World - Timeline {params.id}</h1>
+    </div>
+  );
+}
