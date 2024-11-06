1a4a0608539108c40c923eafdb42b10000bd8166
Suganya <satish860@gmail.com>
2024-11-06 12:35:03 +0530

    Removed the unused Sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import { FeaturesSection } from "@/components/layout/features";
 import { FooterSection } from "@/components/layout/footer";
 import { HeroSection } from "@/components/layout/hero";
-import { TeamSection } from "@/components/layout/team";
-import { TestimonialSection } from "@/components/layout/testimonials";
-import { PricingSection } from "@/components/layout/pricing";
-import { ContactSection } from "@/components/layout/contact";
 import { FAQSection } from "@/components/layout/faq";
 import { Navbar } from "@/components/navbar";
 
@@ -14,10 +10,6 @@ export default function LandingPage() {
       <Navbar />
       <HeroSection />
       <FeaturesSection />
-      {/* <TestimonialSection /> */}
-      {/* <TeamSection />
-      <PricingSection />
-      <ContactSection /> */}
       <FAQSection />
       <FooterSection />
     </>
